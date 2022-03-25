a7b121543c8e59fc3305237fd608c23b4cf0331f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-03-25 15:07:01 -0700

    third commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
+    const $input = $("input")
+    const $button = $("button")
+    const $aside = $("aside") 
 
+    $button.on("click", () => {
+    const searchTerm = $input.val()
 
-
-const $input = $("input")
-const $button = $("button")
-const $aside = $("aside") 
-button.on("click", () => {
-     const searchTerm = $input.val()
 $.ajax({
     url:`https://api.funtranslations.com/translate/dothraki.json?text=${searchTerm}`
 }) .then((data) => {
     console.log(data)
-}
+    console.log(data.value.translate)
+})
+})
 
 
-// use interperlation 
-
-// next step start planning html 
