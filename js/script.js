66e600c0cf97a2f3d20cd0bce17d7990c97497d4
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-03-26 12:17:55 -0700

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-    const $input = $("input")
-    const $button = $("button")
-    const $aside = $("aside") 
+const $input = $("input")
+const $button = $("button")
+const $aside = $("aside")
 
-    $button.on("click", () => {
-    const searchTerm = $input.val()
+$button.on("click", () => {
+    const searchTerm = $input.val();
 
-$.ajax({
-    url:`https://api.funtranslations.com/translate/dothraki.json?text=${searchTerm}`
-}) .then((data) => {
-    console.log(data)
-    console.log(data.value.translate)
+    $.ajax({
+        url: `https://api.funtranslations.com/translate/dothraki.json?text=${searchTerm}`
+    }) .then((data) => {
+         console.log(data)
+    })
+      
+    .catch((error) => {
+        console.log(error)
+    })
 })
-})
-
-
