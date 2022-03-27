bcd2694227ee844949c02f53220132f95ca726fe
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-03-27 15:14:13 -0700

    sixth commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
-const $input = $("input")
-const $button = $("button")
-const $aside = $("aside")
+const btnTranslate = document.querySelector("#translation-button");
+const translationText = document.querySelector("#translation-input");
+const outputText = document.querySelector("#translation-output");
+const btnPlay = document.querySelector("#translation-button-speak");
 
-$button.on("click", () => {
-    const searchTerm = $input.val();
+const res = "https://api.funtranslations.com/translate/dothraki.json";
+const URL = encodeURI(res);
 
-    $.ajax({
-        url: `https://api.funtranslations.com/translate/dothraki.json?text=${searchTerm}`
-    }) .then((data) => {
-         console.log(data)
+
+function getTranslationalURL(text) {
+  return URL + "?" + "text=" + text;
+}
+
+function errorHandler(err) {
+  console.log("error occurred" + err);
+  alert("something wrong with the server! try again after some time");
+}
+
+function textSpeak(text) {
+  responsiveVoice.speak(text, "Italian Female");
+}
+
+function clickHandler() {
+  var inputText = translationText.value;
+  fetch(getTranslationalURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputText.innerText = translatedText;
     })
-      
-    .catch((error) => {
-        console.log(error)
-    })
-})
+    .catch(errorHandler);
+  translationText.value = "";
+}
+
+btnPlay.addEventListener("click", () => {
+  textSpeak(outputText.innerText);
+});
+
+btnTranslate.addEventListener("click", clickHandler);
+
+
+
+
+
+
